330f2831ab3c4de4e3a073641a5b423af6f84878
Bahadır Bör <bahadir.bor.01@gmail.com>
2024-10-07 22:45:01 +0300

    Add project files.

---
+++ 01_Introduction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+            //Console.WriteLine("Hello world");
+
+            #region Introduction
+            Console.WriteLine("***** Yemek Kategorileri *****" + "\n");
+            Console.WriteLine("1-Çorbalar");
+            Console.WriteLine("2-Ana Yemekler");
+            Console.WriteLine("3-Soğuk Başlangıçlar");
+            Console.WriteLine("4-Salatalar");
+            Console.WriteLine("5-Tatlılar");
+            Console.WriteLine("6-İçecekler" + "\n");
+            Console.WriteLine("***** Yemek Kategorileri *****");
+            #endregion
+
+
+        }
+    }
+}
