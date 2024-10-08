68d5e42081717156a6b523620b3bca54602ba05e
Bahadır Bör <bahadir.bor.01@gmail.com>
2024-10-08 19:20:03 +0300

    Created and modified "StringVariables" region

--- 01_Introduction/Program.cs
+++ 01_Introduction/Program.cs
@@ -12,16 +12,47 @@ namespace _01_Introduction
             //Console.WriteLine("Hello world");
 
             #region Introduction
-            Console.WriteLine("***** Yemek Kategorileri *****" + "\n");
-            Console.WriteLine("1-Çorbalar");
-            Console.WriteLine("2-Ana Yemekler");
-            Console.WriteLine("3-Soğuk Başlangıçlar");
-            Console.WriteLine("4-Salatalar");
-            Console.WriteLine("5-Tatlılar");
-            Console.WriteLine("6-İçecekler" + "\n");
-            Console.WriteLine("***** Yemek Kategorileri *****");
+            //Console.WriteLine("***** Yemek Kategorileri *****" + "\n");
+            //Console.WriteLine("1-Çorbalar");
+            //Console.WriteLine("2-Ana Yemekler");
+            //Console.WriteLine("3-Soğuk Başlangıçlar");
+            //Console.WriteLine("4-Salatalar");
+            //Console.WriteLine("5-Tatlılar");
+            //Console.WriteLine("6-İçecekler" + "\n");
+            //Console.WriteLine("***** Yemek Kategorileri *****");
             #endregion
 
+            #region StringVariables
+            string customerName = "Ali";
+            string customerSurname = "Çınar";
+            string customerPhone = "+90 500 400 30 20";
+            string customerMail = "deneme@gmail.com";
+            string district = "Kadıköy";
+            string city = "İstanbul";
+
+            Console.WriteLine("***** Rezervasyon Kartı *****" + "\n");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            Console.WriteLine("İletişim: " + customerPhone);
+            Console.WriteLine("Email Adresi: " + customerMail);
+            Console.WriteLine("Adres: " + district + "/" + city);
+            Console.WriteLine("------------------------------");
+
+            customerName = "Ayşegül";
+            customerSurname = "Kaya";
+            customerPhone = "+90 400 300 20 10";
+            customerMail = "test@gmail.com";
+            district = "Sapanca";
+            city = "Sakarya";
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            Console.WriteLine("İletişim: " + customerPhone);
+            Console.WriteLine("Email Adresi: " + customerMail);
+            Console.WriteLine("Adres: " + district + "/" + city);
+            Console.WriteLine("------------------------------");
+
+            #endregion
 
         }
     }
