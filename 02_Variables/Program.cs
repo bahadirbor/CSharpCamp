f5939a2b53ed5b07df9b52b9949a64b660b419f0
Bahadır Bör <bahadir.bor.01@gmail.com>
2024-10-08 21:08:37 +0300

    Created and completed "CharVariables" region

--- 02_Variables/Program.cs
+++ 02_Variables/Program.cs
@@ -6,10 +6,68 @@ using System.Threading.Tasks;
 
 namespace _02_Variables
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+    internal class Program{
+        static void Main(string[] args){
+            //double number = 4.85;
+
+            #region doubleVariables
+            //Console.WriteLine("***** Fiyat Listesi *****" + "\n");
+
+            //double applePrice, orangePrice, strawberryPrice, potatoPrice, tomatoPrice;
+
+            //applePrice = 14.85;
+            //orangePrice = 20.95;
+            //strawberryPrice = 45;
+            //potatoPrice = 9.74;
+            //tomatoPrice = 6.68;
+
+            //Console.WriteLine("---- Elma Birim Fiyatı: " + applePrice + " TL");
+            //Console.WriteLine("---- Portakal Birim Fiyatı: " + orangePrice + " TL");
+            //Console.WriteLine("---- Çilek Birim Fiyatı: " + strawberryPrice + " TL");
+            //Console.WriteLine("---- Patates Birim Fiyatı: " + potatoPrice + " TL");
+            //Console.WriteLine("---- Domates Birim Fiyatı: " + tomatoPrice + " TL" + "\n");
+
+            //double appleGram, orangeGram, strawberryGram, potatoGram, tomatoGram;
+
+            //appleGram = 1.245;
+            //orangeGram = 2.650;
+            //strawberryGram = 0.750;
+            //potatoGram = 4.859;
+            //tomatoGram = 3.745;
+
+            //double appleTotalPrice = appleGram * applePrice;
+            //double orangeTotalPrice = orangeGram * orangePrice;
+            //double strawberryTotalPrice = strawberryGram * strawberryPrice;
+            //double potatoTotalPrice = potatoGram * potatoPrice;
+            //double tomatoTotalPrice = tomatoGram * tomatoPrice;
+
+            //Console.WriteLine("***** Ürün Listesi *****");
+
+            //Console.WriteLine("Alınan Ürün: Elma - " + "Birim Fiyat: " + applePrice + " - Gramaj: " +
+            //    appleGram + " - Toplam Tutar: " + appleTotalPrice);
+
+            //Console.WriteLine("Alınan Ürün: Portakal - " + "Birim Fiyat: " + orangePrice + " - Gramaj: " +
+            //    orangeGram + " - Toplam Tutar: " + orangeTotalPrice);
+
+            //Console.WriteLine("Alınan Ürün: Çilek - " + "Birim Fiyat: " + strawberryPrice + " - Gramaj: " +
+            //    strawberryGram + " - Toplam Tutar: " + strawberryTotalPrice);
+
+            //Console.WriteLine("Alınan Ürün: Patates - " + "Birim Fiyat: " + potatoPrice + " - Gramaj: " +
+            //    potatoGram + " - Toplam Tutar: " + potatoTotalPrice);
+
+            //Console.WriteLine("Alınan Ürün: Domates - " + "Birim Fiyat: " + tomatoPrice + " - Gramaj: " +
+            //    tomatoGram + " - Toplam Tutar: " + tomatoTotalPrice);
+
+            //double shoppingTotalPrice = appleTotalPrice + orangeTotalPrice + strawberryTotalPrice + potatoTotalPrice +
+            //    tomatoTotalPrice;
+
+            //Console.WriteLine("Toplam Tutar: " + shoppingTotalPrice);
+
+            #endregion
+
+            #region CharVariables
+            
+
         }
     }
 }
