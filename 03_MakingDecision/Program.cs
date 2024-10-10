9d71c2c546c22bdfa0a6894d9769c655555922c5
Bahadır Bör <bahadir.bor.01@gmail.com>
2024-10-10 11:21:09 +0300

    Completed "CharDecisions" region

--- 03_MakingDecision/Program.cs
+++ 03_MakingDecision/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,33 +27,50 @@ namespace _03_MakingDecision
             #endregion
 
             #region ElseIf
-            double exam, exam2, exam3, average = 0;
+            //double exam, exam2, exam3, average = 0;
 
-            Console.Write("Input your exam result: ");
-            exam = double.Parse(Console.ReadLine());
+            //Console.Write("Input your exam result: ");
+            //exam = double.Parse(Console.ReadLine());
 
-            Console.Write("Input your exam result: ");
-            exam2 = double.Parse(Console.ReadLine());
+            //Console.Write("Input your exam result: ");
+            //exam2 = double.Parse(Console.ReadLine());
 
-            Console.Write("Input your exam result: ");
-            exam3 = double.Parse(Console.ReadLine());
+            //Console.Write("Input your exam result: ");
+            //exam3 = double.Parse(Console.ReadLine());
 
-            average = (exam + exam2 + exam3) / 3;
+            //average = (exam + exam2 + exam3) / 3;
 
-            if (average >= 50)
-            {
-                Console.WriteLine("Your average exam score: " + average);
-                Console.WriteLine("You have succeed");
-            }
-            else if (average < 50)
-            {
-                Console.WriteLine("Your average exam score: " + average);
-                Console.WriteLine("You have failed");
-            }
-            else {
-                Console.WriteLine("ERROR");
-            }
+            //if (average >= 50)
+            //{
+            //    Console.WriteLine("Your average exam score: " + average);
+            //    Console.WriteLine("You have succeed");
+            //}
+            //else if (average < 50)
+            //{
+            //    Console.WriteLine("Your average exam score: " + average);
+            //    Console.WriteLine("You have failed");
+            //}
+            //else {
+            //    Console.WriteLine("ERROR");
+            //}
             #endregion
+
+            #region CharDecisions
+            //char team;
+            //Console.Write("Takım Sembolü girin: ");
+            //team = char.Parse(Console.ReadLine());
+
+            //if(team == 'g' |  team == 'G'){
+            //    Console.WriteLine("Galatasaray");
+            //}
+            //else if(team == 'b' |  team == 'B'){
+            //    Console.WriteLine("Beşiktaş");
+            //}
+            //else if(team == 'f' | team == 'F'){
+            //    Console.WriteLine("Fenerbahçe");
+            //}
+            #endregion
+
         }
     }
 }
