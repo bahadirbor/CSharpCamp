66266f2b11d9e57d37fc58d9b82286d9af2faa4b
Bahadır Bör <bahadir.bor.01@gmail.com>
2024-10-10 11:09:57 +0300

    Created "MakingDecision" project. Completed "If" and "ElseIf" region.

---
+++ 03_MakingDecision/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_MakingDecision
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+            #region If
+            /*string password;
+
+            Console.Write("Insert your password: ");
+            password = Console.ReadLine();
+
+            if (password == "abcd")
+            {
+                Console.WriteLine("Password is correct!");
+            }
+            else
+            {
+                Console.WriteLine("Password is incorrect or innvalid");
+            }*/
+            #endregion
+
+            #region ElseIf
+            double exam, exam2, exam3, average = 0;
+
+            Console.Write("Input your exam result: ");
+            exam = double.Parse(Console.ReadLine());
+
+            Console.Write("Input your exam result: ");
+            exam2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Input your exam result: ");
+            exam3 = double.Parse(Console.ReadLine());
+
+            average = (exam + exam2 + exam3) / 3;
+
+            if (average >= 50)
+            {
+                Console.WriteLine("Your average exam score: " + average);
+                Console.WriteLine("You have succeed");
+            }
+            else if (average < 50)
+            {
+                Console.WriteLine("Your average exam score: " + average);
+                Console.WriteLine("You have failed");
+            }
+            else {
+                Console.WriteLine("ERROR");
+            }
+            #endregion
+        }
+    }
+}
