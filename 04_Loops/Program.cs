d2182682ed8f64639855ce8ddd2d00ffdaf8fa95
Bahadır Bör <bahadir.bor.01@gmail.com>
2024-10-11 09:12:37 +0300

    Coding "ForLoop", "WhileLoop", "Project" region

--- 04_Loops/Program.cs
+++ 04_Loops/Program.cs
@@ -9,6 +9,42 @@ namespace _04_Loops
     internal class Program
     {
         static void Main(string[] args){
+            #region ForLoop
+            //for(int i  = 0; i < 20; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            #endregion
+
+            #region WhileLoop
+            //int j = 0;
+            //int bacterium = 2;
+
+            //while (j < 16)
+            //{
+            //    Console.WriteLine(j + " saat sonunda bakteri sayısı " + bacterium);
+            //    bacterium *= 2;
+            //    j++;
+            //}
+            #endregion
+
+            #region Project
+            //int number, bsm, result = 0;
+
+            //Console.Write("Bir sayı girin: ");
+            //number = int.Parse(Console.ReadLine());
+
+            //while (number > 0)
+            //{
+            //    bsm = number % 10;
+            //    result += bsm;
+            //    number /= 10;
+            //}
+            //Console.WriteLine(result);
+            #endregion
 
         }
     }
