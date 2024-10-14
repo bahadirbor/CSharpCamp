d2f100b26e7cc1665271efbf061671e91774644d
Bahadır Bör <bahadir.bor.01@gmail.com>
2024-10-14 12:10:01 +0300

    Completed five examples

--- 05_LoopsWithStars/Program.cs
+++ 05_LoopsWithStars/Program.cs
@@ -8,8 +8,56 @@ namespace _05_LoopsWithStars
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
+
+            #region SampleOne
+            //for(int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+            #endregion
+
+            #region SampleTwo
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+            #endregion
+
+            #region SampleThree
+            //for(int j = 0; j < 10; j++)
+            //{
+            //    for(int i = 0; i < 10; i++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.Write("\n");
+            //}
+
+            #endregion
+
+            #region SampleFour
+            //for(int i = 1; i <= 10; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.Write("\n");
+            //}
+            #endregion
+
+            #region SampleFive
+            //for (int i = 10; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.Write("\n");
+            //}
+            #endregion
+
         }
     }
 }
