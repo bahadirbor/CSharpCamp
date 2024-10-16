98f2e91bcbd2bdb20feba8fd97c206f53ead2995
Bahadır Bör <bahadir.bor.01@gmail.com>
2024-10-16 22:17:52 +0300

    Completed "Arrays" project

--- 06_Arrays/Program.cs
+++ 06_Arrays/Program.cs
@@ -8,8 +8,26 @@ namespace _06_Arrays
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
+            //string[] colours = {"Kırmızı","Beyaz","Siyah"};
+
+            //Console.WriteLine(colours[2]);
+
+            #region Sample
+            string[] cities = new string[5];
+
+            for (int i = 0; i < cities.Length; i++) { 
+                Console.Write("Enter a city name: ");
+                cities[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("------------------------------------");
+
+            for(int i = 0;i < cities.Length; i++) { 
+                Console.WriteLine(cities[i]); 
+            }
+            #endregion
         }
     }
 }
