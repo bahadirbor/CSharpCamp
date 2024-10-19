510c399b7946e973016e24b1352c86a766384d95
Bahadır Bör <bahadir.bor.01@gmail.com>
2024-10-19 15:19:18 +0300

    Completed "Methods" project

--- 08_Methods/Program.cs
+++ 08_Methods/Program.cs
@@ -8,8 +8,34 @@ namespace _08_Methods
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
+
+            //void hello()
+            //{
+            //    Console.WriteLine("Hello");
+            //}
+
+            //hello();
+
+            // -------------------------------------------------------
+
+            //double average(double num1, double num2, double num3)
+            //{
+            //    double sum = num1 + num2 + num3;
+            //    return sum / 3;
+            //}
+
+            //Console.WriteLine(average(45, 55, 65));
+
+            // --------------------------------------------------------
+
+            //string hellowname(string name)
+            //{
+            //    return "hello " + name;
+            //}
+
+            //Console.WriteLine(hellowname("optimus"));
+
         }
     }
 }
