d4be15f5381d9ac527c7db62982e3582ad3ca3f4
Bahadır Bör <bahadir.bor.01@gmail.com>
2024-10-31 21:45:46 +0300

    Completed "DatabaseProject"

--- 09_DatabaseProject/Program.cs
+++ 09_DatabaseProject/Program.cs
@@ -1,15 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace _09_DatabaseProject
 {
     internal class Program
     {
         static void Main(string[] args){
+            Console.WriteLine("***** C# Restaurant Menu *****");
+            Console.WriteLine("\n" + "---------------------------------" + "\n");
+            Console.WriteLine("1 - Soupes");
+            Console.WriteLine("2 - Hot Appetizer");
+            Console.WriteLine("3 - Main Course");
+            Console.WriteLine("4 - Desserts");
+            Console.WriteLine("5 - Salads");
+            Console.WriteLine("6 - Beverages");
+            Console.WriteLine("\n" + "---------------------------------");
 
+            Console.Write("Write your category number: ");
+            int kategoryNumber = int.Parse(Console.ReadLine());
+
+            SqlConnection connect = new SqlConnection(/*"Insert your database name"*/);
+            connect.Open();
+
+            SqlCommand readTable = new SqlCommand(/*Insert your query*/, connect);
+            SqlDataAdapter adapter = new SqlDataAdapter(readTable);
+            DataTable dataTablo = new DataTable();
+            adapter.Fill(dataTablo);
+
+            connect.Close();
+
+            foreach(DataRow row in dataTablo.Rows)
+            {
+                foreach(var a in row.ItemArray)
+                {
+                    Console.Write(a.ToString());
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
