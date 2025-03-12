15fa75bf85b761298e4bfbd08306ae7d2ea14b56
Bahadır Bör <bahadir.bor.01@gmail.com>
2025-03-12 17:41:12 +0300

    Created user function

---
+++ ATM/Users.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    internal class Users
+    {
+        // Private variables
+        private string Name;
+        private string Surname;
+        private int Balance = 0;
+
+        //Constructor
+        public Users(string name, string surname, int balance)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Balance = balance;
+        }
+
+        // Variable get functions
+        internal string getName() { return Name; }
+        internal string getSurname() { return Surname; }
+        internal int getBalance() { return Balance; }
+
+        // Variable set functions
+        internal void setName(string newName) { Name = newName; }
+        internal void setSurname(string newSurname) { Surname = newSurname; }
+        internal void setBalance(int newBalance) { Balance = newBalance; }
+
+    }
+}
