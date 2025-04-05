9992493809debba263fe234f2f52424c1705ff20
Bahadır Bör <bahadir.bor.01@gmail.com>
2025-04-05 23:44:55 +0300

    Completed register section. New users can register with the bank. Bank customers will be able to log in to their accounts.

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -7,13 +7,38 @@ using System.Threading.Tasks;
 
 namespace ATM
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            
+    internal class Program{
 
+        static void Main(string[] args){
             
+            while (true){
+                Console.WriteLine("Welcome to the ATM!");
+                Console.Write("Press 1 for register/Press 2 for login:");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1){
+                    Console.Write("Please enter your name:");
+                    string name = Console.ReadLine();
+                    Console.Write("Please enter your surname:");
+                    string surname = Console.ReadLine();
+                    Console.Write("Please enter your balance:");
+                    int balance = Convert.ToInt32(Console.ReadLine());
+
+                    Users user = new Users(name, surname, balance);
+                    
+                    Console.WriteLine("Registering...");
+                    Console.WriteLine($"User {user.getName()} {user.getSurname()} with balance {user.getBalance()} registered successfully.");
+                }
+                else if (choice == 2){
+                    Console.WriteLine("Logging in...");
+                    break;
+                }
+                else{
+                    Console.WriteLine("Invalid choice. Please try again.");
+                }
+            }
+
+
 
         }
     }
